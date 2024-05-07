eaf6ce4a9d8ad5153e2f3ace7fe3ff5c387886fb
Guillaume Fricker <fricker.g@zoho.com>
2024-05-07 23:10:05 +0200

    remove cardsToShow state

--- src/components/layouts/Content/Content.tsx
+++ src/components/layouts/Content/Content.tsx
@@ -11,7 +11,6 @@ export type SortDirection = 'asc' | 'desc'
 
 function Content() {
   const [cards, setCards] = useState<Card[]>(baseData)
-  const [cardsToShow, setCardsToShow] = useState<Card[]>([...cards])
   const [sortType, setSortType] = useState<InputType>('front')
   const [sortDirection, setSortDirection] = useState<SortDirection>('asc')
   const [cardInput, setCardInput] = useState({ front: '', back: '' })
@@ -32,35 +31,6 @@ function Content() {
   const handleDeleteById = (id: number) => {
     const updatedCards = cards.filter(card => card.id !== id)
     setCards(updatedCards)
-
-    if (filterChecked) {
-      setCardsToShow(
-        updatedCards.filter(
-          card =>
-            card.front.toLowerCase().includes(cardInput.front.toLowerCase()) &&
-            card.back.toLowerCase().includes(cardInput.back.toLowerCase())
-        )
-      )
-    } else {
-      setCardsToShow(updatedCards)
-    }
-  }
-
-  const handleInputChange = (inputType: InputType, value: string) => {
-    const updatedInput = { ...cardInput, [inputType]: value }
-    setCardInput(updatedInput)
-
-    const { front, back } = updatedInput
-    if (filterChecked) {
-      const matchingCards = cards.filter(
-        card =>
-          (!front || card.front.toLowerCase().includes(front.toLowerCase())) &&
-          (!back || card.back.toLowerCase().includes(back.toLowerCase()))
-      )
-      setCardsToShow(matchingCards)
-    } else {
-      setCardsToShow(cards)
-    }
   }
 
   const handleAddNewCard = () => {
@@ -73,26 +43,29 @@ function Content() {
       setNextId(nextId + 1)
       setCardInput({ front: '', back: '' })
       setCards([...cards, newCard])
-      setCardsToShow([...cards, newCard])
     }
+  }
+
+  const handleInputChange = (inputType: InputType, value: string) => {
+    const updatedInput = { ...cardInput, [inputType]: value }
+    setCardInput(updatedInput)
   }
 
   const handleCheckboxChange = (value: boolean) => {
     setFilterChecked(value)
-    if (value) {
-      const matchingCards = cards.filter(
+  }
+
+  const directionMultiplier = sortDirection === 'asc' ? 1 : -1
+
+  const filteredCards = filterChecked
+    ? cards.filter(
         card =>
           card.front.toLowerCase().includes(cardInput.front.toLowerCase()) &&
           card.back.toLowerCase().includes(cardInput.back.toLowerCase())
       )
-      setCardsToShow(matchingCards)
-    } else {
-      setCardsToShow(cards)
-    }
-  }
+    : cards
 
-  const directionMultiplier = sortDirection === 'asc' ? 1 : -1
-  const sortedCardsToShow = [...cardsToShow].sort(
+  const sortedCardsToShow = [...filteredCards].sort(
     (a, b) => a[sortType].localeCompare(b[sortType]) * directionMultiplier
   )
 

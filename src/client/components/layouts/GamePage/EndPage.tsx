25b246330cceb20db9ab0b12b7cb16552f40fff6
Jvan Fadda <jvan.fadda@escatec.com>
2024-05-25 16:19:25 +0200

    Fix: End-Page naming adjusted

--- src/client/components/layouts/GamePage/EndPage.tsx
+++ src/client/components/layouts/GamePage/EndPage.tsx
@@ -1,12 +1,12 @@
 // TODO man könnte Label anstelle von h2 und p verwenden
 
-import './GamePage.css'
+import './EndPage.css'
 import { Fragment, useContext } from 'react'
 import { GameContext } from '../../../../api/GameContext'
 import Button from '../../elements/Button/Button'
 import { useNavigate } from 'react-router-dom'
 
-export default function End() {
+export default function EndPage() {
   const { state, dispatch } = useContext(GameContext)
   const { cards } = state
   const navigate = useNavigate()
@@ -17,10 +17,10 @@ export default function End() {
   }
 
   return (
-    <div className="game-page-results">
+    <div className="end-page-results">
       <Button
         label="Start New Game"
-        className="game-page-button"
+        className="end-page-button"
         onClick={() => startNewGame()}
       />
       <h2>Game Results</h2>
@@ -28,17 +28,17 @@ export default function End() {
         Solved {cards.filter(card => card.accepted).length} out of{' '}
         {cards.length} correctly.
       </p>
-      <div className="game-page-container">
-        <div className="game-page-header">Front</div>
-        <div className="game-page-header">Back</div>
-        <div className="game-page-header">Your Answer</div>
-        <div className="game-page-header">Accepted</div>
+      <div className="end-page-container">
+        <div className="end-page-header">Front</div>
+        <div className="end-page-header">Back</div>
+        <div className="end-page-header">Your Answer</div>
+        <div className="end-page-header">Accepted</div>
         {cards.map(card => (
           <Fragment key={card.id}>
-            <div className="game-page-item">{card.front}</div>
-            <div className="game-page-item">{card.back}</div>
-            <div className="game-page-item">{card.answer}</div>
-            <div className="game-page-item">{card.accepted ? '✓' : '✗'}</div>
+            <div className="end-page-item">{card.front}</div>
+            <div className="end-page-item">{card.back}</div>
+            <div className="end-page-item">{card.answer}</div>
+            <div className="end-page-item">{card.accepted ? '✓' : '✗'}</div>
           </Fragment>
         ))}
       </div>

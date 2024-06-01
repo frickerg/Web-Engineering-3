88870a3813170884960494dcf14fc5d3872ef6e4
Guillaume Fricker <fricker.g@zoho.com>
2024-06-01 15:22:51 +0200

    styling cleanup

--- src/client/components/layouts/GamePage/Ongoing.tsx
+++ src/client/components/layouts/GamePage/Ongoing.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom'
 import { FlashcardProps } from '../../../../model/Card'
 import Input from '../../elements/Input/Input'
 import Flashcard from '../../elements/Flashcard/Flashcard'
+import Label from '../../elements/Label/Label'
 
 function mapCardToGameResultItem(cards: FlashcardProps[]): GameResultItem[] {
   return cards.map(card => ({
@@ -25,8 +26,12 @@ export default function Ongoing() {
   const [answer, setAnswer] = useState('')
   const navigate = useNavigate()
 
-  const progress =
-    cards.length > 0 ? Math.round((index / cards.length) * 100) : 0
+  const progressLabel = () => {
+    const progress =
+      cards.length > 0 ? Math.round((index / cards.length) * 100) : 0
+
+    return `Progress: ${progress}%`
+  }
 
   useEffect(() => {
     const fetchCards = async () => {
@@ -85,13 +90,13 @@ export default function Ongoing() {
   }
 
   return (
-    <div className="ongoing-page">
+    <div className="ongoing-container">
       <div className="ongoing-header">
-        <div className="ongoing-progress">Progress: {progress}%</div>
+        <Label label={progressLabel()} className="progress-label" />
         <Button
           label="Delete Game"
           onClick={handleDeleteGame}
-          className="ongoing-delete-button"
+          className="delete-button"
         />
       </div>
       <Flashcard text={cards[index]?.front} />

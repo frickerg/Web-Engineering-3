914ad474425c236b657554a1b49c818fe5300e5f
Jvan Fadda <jvan.fadda@escatec.com>
2024-06-02 10:45:08 +0200

    fixed CenterButton-postition to center of app-bar changed; changed style to styled-components

--- src/client/components/layouts/AppBar/Appbar.tsx
+++ src/client/components/layouts/AppBar/Appbar.tsx
@@ -1,15 +1,37 @@
 import { Link } from 'react-router-dom'
-import './Appbar.css'
+import styled from 'styled-components'
 import CenterButton from './CenterButton'
+import Label from '../../elements/Label/Label'
 
 function Appbar() {
   return (
-    <div className="top-banner flex-row">
-      <h1>Mimir</h1>
+    <AppbarContainer>
+      <Title label="Mimir"></Title>
       <CenterButton />
-      <Link to="/cards">Manage Cards</Link>
-    </div>
+      <StyledLink to="/cards">Manage Cards</StyledLink>
+    </AppbarContainer>
   )
 }
 
 export default Appbar
+
+const AppbarContainer = styled.div`
+  background-color: #182d4a;
+  color: #fefefe;
+  padding: 20px 15px;
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  align-items: center;
+`
+
+const Title = styled(Label)`
+  font-size: 30px;
+  font-weight: lighter;
+  color: inherit;
+`
+
+const StyledLink = styled(Link)`
+  justify-self: end;
+  color: inherit;
+  text-decoration: none;
+`

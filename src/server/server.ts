2333ad5508f792c8bd16dc773d6427404e65234e
Jvan Fadda <jvan.fadda@escatec.com>
2024-09-15 11:41:12 +0200

    feat(pw-gen): add password hash generation API endpoint
    - Added a new API endpoint `/api/password-generator/:password` to generate a hashed password using the `crypto` module.
    - The endpoint uses `crypto.scrypt` to create a secure hash with a random salt and responds with both the hashed password and the salt.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cardsRoutes from './routes/index'
+import crypto from 'crypto'
 
 const app = express()
 app.use(express.json())
@@ -9,4 +10,15 @@ app.use('/api/', cardsRoutes)
 const port = 3003
 app.listen(port, () => {
   console.info(`Server is running on port ${port}`)
+})
+
+app.get('/api/password-generator/:password', async (req, res) => {
+  const password = req.params.password as string
+  const salt = crypto.randomBytes(16).toString('hex')
+  crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+    if (err) throw err
+    const passwordHash = derivedKey.toString('hex')
+    console.log(`Password hash: ${passwordHash}, salt: ${salt}`)
+    res.status(200).send({ passwordHash, salt })
+  })
 })

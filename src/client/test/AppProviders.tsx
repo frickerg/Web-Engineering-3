da735b61c56e5db0f707fa1de062fbebcf891bbf
Jvan Fadda <jvan.fadda@escatec.com>
2024-10-02 08:00:20 +0200

    fix(AppProvider): removed Mantine dependency

--- src/client/test/AppProviders.tsx
+++ src/client/test/AppProviders.tsx
@@ -1,4 +1,3 @@
-import { createTheme, MantineProvider } from '@mantine/core'
 import {
   Fragment,
   PropsWithChildren,
@@ -9,25 +8,6 @@ import {
 import { Outlet } from 'react-router-dom'
 import { fetchCards } from '../api'
 import { GameContext, initialState, reducer } from '../session/GameContext'
-
-const theme = createTheme({
-  autoContrast: true,
-  colors: {
-    todo: [
-      '#e3fff0',
-      '#d1fae3',
-      '#a5f3c8',
-      '#76ebab',
-      '#50e592',
-      '#36e182',
-      '#24df79',
-      '#10c668',
-      '#00b05a',
-      '#00984a',
-    ],
-  },
-  primaryColor: 'todo',
-})
 
 export const AppProviders = ({ children }: PropsWithChildren) => {
   const { dispatch } = useContext(GameContext)

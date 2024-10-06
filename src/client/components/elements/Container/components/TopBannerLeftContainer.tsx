053ffd85b4d258193e6ca5e81fb4630a273d7590
g0tcoffee <benjamin.thormann@ost.ch>
2024-10-06 06:25:56 +0200

    fixed mobile header behaviour

--- src/client/components/elements/Container/components/TopBannerLeftContainer.tsx
+++ src/client/components/elements/Container/components/TopBannerLeftContainer.tsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components'
 import { Container } from '../Container'
-import { viewportDevice } from '../../../../themes/Breakpoints'
 
 export const TopBannerLeftContainer = styled(Container)`
-  @media (${viewportDevice.desktop}) {
     grid-area: head;
     display: flex;
     align-items: baseline;
-  }
+  
 `
